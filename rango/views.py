10e1422dec67404dd74f3088f0957377521f0ba7
Fuyu JIA <2494182J@student.gla.ac.uk>
2021-07-30 05:36:21 +0100

    This is chapter 6, Models, Templates and Views

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
 
-# Construct a dictionary to pass to the template engine as its context.
-# Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict= {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-# Return a rendered response to send to the client.
-# We make use of the shortcut function to make our lives easier.
-# Note that the first parameter is the template we wish to use.
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    
+
     return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
     # no need context_dict= {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+    try:
+# Can we find a category name slug with the given name?
+
+        category = Category.objects.get(slug=category_name_slug)
+# Retrieve all of the associated pages.
+
+        pages = Page.objects.filter(category=category)
+# Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+# We also add the category object from
+
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+# We get here if we didn't find the specified category.
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+# Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context=context_dict)
